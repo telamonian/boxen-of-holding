f4ba7d010ae30d287eb22a306dbe683645eae2ee
telamonian <mklein@jhu.edu>
2014-08-27 20:07:54 -0400

    switched from using boxen-linux to our-boxen/add-linux-support-dev

--- src/darwin/30_darwin_boxen_setup.sh
+++ src/darwin/30_darwin_boxen_setup.sh
@@ -3,7 +3,7 @@ set -e
 /usr/bin/sudo -E -p "need password for sudo:" /bin/mkdir -p /opt/boxen
 /usr/bin/sudo -E -p "need password for sudo:" /usr/sbin/chown "${USER}":staff /opt/boxen
 touch /opt/boxen/.metadata_never_index
-git clone https://github.com/telamonian/boxen-linux.git /opt/boxen/repo
+git clone -b add-linux-support-dev  https://github.com/telamonian/our-boxen.git /opt/boxen/repo
 cd /opt/boxen/repo
 
 set +e
--- src/ubuntu/30_ubuntu_boxen_setup.sh
+++ src/ubuntu/30_ubuntu_boxen_setup.sh
@@ -4,10 +4,9 @@ set -e
 /usr/bin/sudo -E -p "need password for sudo:" /usr/bin/apt-get -y install git-core
 /usr/bin/sudo -E -p "need password for sudo:" /bin/mkdir -p /opt/boxen
 /usr/bin/sudo -E -p "need password for sudo:" /bin/chown "${USER}":${USER} /opt/boxen
-git clone https://github.com/telamonian/boxen-linux.git /opt/boxen/repo
-#git clone -b dev https://github.com/telamonian/boxen-linux.git /opt/boxen/repo
+git clone -b add-linux-support-dev https://github.com/telamonian/our-boxen.git /opt/boxen/repo
 cd /opt/boxen/repo
 
 set +e
 script/bootstrap
-script/boxen --no-fde --debug
+#script/boxen --no-fde --debug
